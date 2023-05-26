590f8c47b3049e55eb4f54c3806087d42281da42
[Eduardo Guimaraes] <[eduardobandeiramg@gmail.com]>
2023-05-26 19:04:12 -0300

    iniciando com colecoes

---
+++ 6- Colecoes - Arrays-Tuplas-Dicio.playground/Contents.swift
@@ -0,0 +1,100 @@
+                                                                    //ARRAYS:
+//Existem diversas formas de declarar e inicializar variáveis contendo listas:
+var exemplo1: [Double]
+var exemplo2: [Double] = []
+var exemplo3: [Double] = [1.0 , 2.0]
+var exemplo4 = [5.0]
+var exemplo5 : [Double] = Array<Double>()
+var exemplo6 : [Double] = Array()
+var exemplo7 = Array<Double>()
+var exemplo8: [Double] = Array(arrayLiteral: 1.0)
+var exemplo9 = Array<Double>(arrayLiteral: 1.0)
+var exemplo10 = Array(arrayLiteral: 1.0)
+var exemplo11 = [Double]()
+//etc etc etc...
+
+//Principais funções e propriedades de arraya: (retornam valores opcionais)
+exemplo2.append(1.0)
+exemplo2.append(2.0)
+exemplo2.append(3.0)
+exemplo2.append(4.0)
+exemplo2.append(5.0)
+exemplo2.insert(3.5, at: 3)
+exemplo2.remove(at:4) //existem varios outros removes
+exemplo2.first //existem vários outros a partir de first, como firstIndexOf etc
+exemplo2.last
+exemplo2.count
+exemplo2.min()
+exemplo2.max()
+exemplo2.contains(2.0)
+print(exemplo2.enumerated())
+
+//Percorrendo Arrays:
+for controle in exemplo2{
+    print(controle)
+}
+print()
+
+for i in 0..<exemplo2.count{
+    print(exemplo2[i])
+}
+print()
+
+for(index , elemento) in exemplo2.enumerated(){
+    print(index , elemento)
+}
+print()
+
+                                                                    //SETS (CONJUNTOS)
+//Como conjuntos, os sets são não ordenados e sem repetição
+//Não é possível acessar posições em conjuntos
+var conjunto : Set<Int> = [0]
+conjunto.insert(1)
+conjunto.insert(2)
+conjunto.insert(3)
+conjunto.insert(4)
+conjunto.insert(5)
+conjunto.insert(5)
+conjunto.insert(5)
+print("Conjunto: \(conjunto)")
+print()
+
+conjunto.contains(4)
+conjunto.remove(4)
+conjunto.remove(7)
+print(conjunto.remove(2))
+print()
+
+                                                                    //TUPLAS
+//Conjunto de elementos de diferentes tipos que podem apresentar chave-valor. São basicamente arryas cujos elementos podem ser de tipos diversos e que também podem ser identificados por chaves
+var aluno : (matricula:Int , nome:String)
+aluno = (1000 , "Eduardo")
+aluno = (matricula:1001 , nome: "Francisco")
+
+var notas = ("Eduardo" , 6 , 7 , 9 , 10)
+var notas2 = (nome: "Eduardo" , nota1: 10 , nota2: 15 , nota3:8)
+
+typealias Alunos = (Int , String)
+let aluno1 : Alunos = (0 , "Eduardo")
+let aluno2 : Alunos = (1 , "Francisco")
+let aluno3 : Alunos = (matricula: 3 , nome: "Maria")
+print("Alunos:")
+print(aluno1.1)
+print(aluno2.1)
+print(aluno3.1)
+
+typealias Inteiros = Int
+var int1 : Inteiros = 0
+var int2 : Inteiros = 1
+var int3 : Inteiros = 2
+var int4 : Inteiros = 3
+
+//Desestruturação em Tuplas;
+var (a , b) = aluno1
+print(a)
+print(b)
+
+var(_ , d) = aluno2
+print(d)
+                                                                    //Dicionários
+
